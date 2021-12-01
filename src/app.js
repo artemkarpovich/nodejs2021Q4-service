2650afda779bd6110b87bbad5c0c764800e845ab
Artsiom <karpartem94@gmail.com>
2021-12-01 16:56:35 +0300

    feat: set up swagger

--- src/app.js
+++ src/app.js
@@ -2,6 +2,16 @@ const fastify = require('fastify')({
   logger: true,
 });
 
+fastify.register(require('fastify-swagger'), {
+  exposeRoute: true,
+  routePrefix: '/docs',
+  swagger: {
+    info: {
+      title: 'competitor-api',
+    },
+  },
+});
+
 const userRoutes = require('./resources/users/user.router');
 
 fastify.register(userRoutes);
